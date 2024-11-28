b64a818cfd3944b629109026d021173fadf6f930
Fausto Núñez Alberro <fausto.nunez@mailbox.org>
2024-11-28 16:57:06 +0100

    Fix unwanted type declarations in output & exclude peer deps from build

--- vite.config.ts
+++ vite.config.ts
@@ -3,13 +3,16 @@ import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
 import dts from "vite-plugin-dts";
 
-export default defineConfig(({ mode }) => {
+export default defineConfig(() => {
   return {
-    plugins: [react(), dts()],
+    plugins: [react(), dts({ exclude: ["node_modules", "dist", "**/*.test.ts"] })],
     build: {
       lib: {
         entry: "./src/index.ts",
         formats: ["es"],
+      },
+      rollupOptions: {
+        external: ["react", "react-dom", "zod"],
       },
     },
     resolve: {
